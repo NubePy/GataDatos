002c685e2ff247ef99b8e473b4d31f6f5aa8cdef
NubePy <139734552+NubePy@users.noreply.github.com>
2023-07-21 02:21:08 -0400

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -9,7 +9,7 @@ _muptime = await new Promise(resolve => { process.once('message', resolve)
 setTimeout(resolve, 2000) }) * 1000}
 
 let uptime = clockString(_uptime)
-let bio = `${global.packname} ⁝⁝  ${uptime} ${lenguajeGB.lenguaje() == 'es' ? '.' : '.'} .`
+let bio = `${global.packname} ⁝⁝ ${lenguajeGB.lenguaje() == 'es' ? '.' : '.'} .`
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 }
@@ -20,5 +20,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' » ', h, ' ・ ', m, ' ・ ', s].map(v => v.toString().padStart(2, 0)).join('') 
+  return [d, '.', h, '.', m, '.', s].map(v => v.toString().padStart(2, 0)).join('') 
 } 
